d3438fbbbc4bb012cc56bc40356a1b1107802b03
Ralf Becker <RalfBecker@outdoor-training.de>
2017-05-04 22:59:22 +0200

    update to MariaDB 10.1.23 and had to use "yum update" in Dockerfile to quay.io to longer complain about security fulnarabilities

--- k8s-mariadb-galera-centos/build.sh
+++ k8s-mariadb-galera-centos/build.sh
@@ -16,22 +16,20 @@ IMAGE=galera
 # yum does NOT work, as Dockerfile sets USER 27
 YUM="docker run --rm --entrypoint /usr/bin/yum $REPO/$IMAGE"
 test -z "${TAG:=$1}" && {
-	echo "No tag specified, can determine automatic, use eg. build.sh 10.1.21"
-	exit 1
-	TAG=$($YUM info MariaDB-server|grep '^Version'|tail -1|cut -b 15- || echo -n "7.0")
+	TAG=$(curl https://yum.mariadb.org/10.1/centos7-amd64/rpms/ 2>/dev/null|grep centos7-x86_64-server.rpm|sed -E 's/^.*MariaDB-(10.1.[0-9]+)-centos7-x86_64-server.rpm.*$/\1/g')
 }
 echo -e "\nbuilding $REPO/$IMAGE:$TAG\n"
 
 # check if any packages have updates and update timestamp in Dockerfile to force update
 #$YUM check-update || \
 {
-	sed -i "s/#DATE:.*/#DATE: $(LANG=C date)/" Dockerfile
+	sed -i "" "s/#Date:.*/#Date: $(LANG=C date)/" Dockerfile
 	# pull newer image, to get a small as possible images
 	docker pull centos:7
 }
 
 # repo build
-docker build -t $REPO/$IMAGE:$TAG .
+docker build --no-cache -t $REPO/$IMAGE:$TAG .
 docker tag $REPO/$IMAGE:$TAG $REPO/$IMAGE:latest
 docker push $REPO/$IMAGE:$TAG
 docker push $REPO/$IMAGE:latest
