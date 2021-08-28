5277537c63e5b3e1186bca2a56b0fe3ed749904f
Ralf Becker <RalfBecker@outdoor-training.de>
2021-08-28 09:21:10 +0200

    changes for MariaDB 10.4

--- k8s-mariadb-galera-centos/build.sh
+++ k8s-mariadb-galera-centos/build.sh
@@ -16,7 +16,7 @@ IMAGE=galera
 # yum does NOT work, as Dockerfile sets USER 27
 YUM="docker run --rm --entrypoint /usr/bin/yum $REPO/$IMAGE"
 test -z "${TAG:=$1}" && {
-	TAG=$(curl https://yum.mariadb.org/10.4/centos7-amd64/rpms/ 2>/dev/null|grep MariaDB-server-10.4|sed -E 's/^.*MariaDB-server-(10.4.[0-9]+).*$/\1/g'|php -r '$vers=file("php://stdin"); usort($vers,"version_compare"); echo implode("",$vers);'|tail -1)
+	TAG=$(curl https://yum.mariadb.org/10.5/centos7-amd64/rpms/ 2>/dev/null|grep MariaDB-server-10.5|sed -E 's/^.*MariaDB-server-(10.5.[0-9]+).*$/\1/g'|php -r '$vers=file("php://stdin"); usort($vers,"version_compare"); echo implode("",$vers);'|tail -1)
 }
 echo -e "\nbuilding $REPO/$IMAGE:$TAG\n"
 
@@ -29,7 +29,7 @@ echo -e "\nbuilding $REPO/$IMAGE:$TAG\n"
 }
 
 # repo build
-docker build -t $REPO/$IMAGE:$TAG .
+docker build --no-cache -t $REPO/$IMAGE:$TAG .
 docker tag $REPO/$IMAGE:$TAG $REPO/$IMAGE:latest
 docker push $REPO/$IMAGE:$TAG
 docker push $REPO/$IMAGE:latest
